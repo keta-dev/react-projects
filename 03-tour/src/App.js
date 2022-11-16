7cb98861154cb5764d81aaf4f7b17fe477af7e78
keta-dev <89269904+keta-dev@users.noreply.github.com>
2022-11-16 21:00:12 +0100

    job tabs

--- 03-tour/src/App.js
+++ 03-tour/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
-console.log(url)
+
+// console.log(url)
 function App() {
   return <h2>Tours Project Setup</h2>
 }
