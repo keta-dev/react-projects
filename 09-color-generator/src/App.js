e3c58e0ef3533ddd7b3a7246521b7866569a99c5
keta-dev <89269904+keta-dev@users.noreply.github.com>
2022-11-18 14:17:54 +0100

    color generator

---
+++ 09-color-generator/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  return (
+    <SingleColor />
+  )
+}
+
+export default App
