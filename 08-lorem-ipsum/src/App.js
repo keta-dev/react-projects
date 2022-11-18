61571d0b71c71ebce152b00a52b08abb96604dcd
keta-dev <89269904+keta-dev@users.noreply.github.com>
2022-11-18 13:07:47 +0100

    start file

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -2,21 +2,35 @@ import React, { useState } from 'react';
 import data from './data';
 
 function App() {
-  const [lorem, setLorem] = useState(data);
+  const [lorem, setLorem] = useState([]);
   const [count, setCount] = useState(0);
-  console.log(lorem)
-  const checkMate = lorem.length;
-  console.log(checkMate);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) amount = 1;
+    if (count > data.length) amount = data.length;
+    setLorem(data.slice(0, amount));
+  }
+
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor='amount'>paragraphs:</label>
-        <input type="text" placeholder='number please...' />
+        <input
+          type="number" 
+          name="amount"
+          placeholder='number please...'
+          value={count}
+          onChange={(e) => setCount(e.target.value)} 
+        />
         <button className="btn">generate</button>
       </form>
       <article className="lorem-text">
-        <p>lorem text</p>
+        {lorem.map((text, i) => (
+          <p key={i}>{text}</p>
+        ))}
       </article>
     </section>
   )
