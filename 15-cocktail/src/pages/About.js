775d78494edbe4fa6faa12a3b2fe9c54fdb58539
keta-dev <89269904+keta-dev@users.noreply.github.com>
2022-12-05 13:22:26 +0100

    navbar and contextApi setup

---
+++ 15-cocktail/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">
+        about us
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et labore error ipsum veritatis dolore impedit odit nihil illum cupiditate fugit nostrum perferendis voluptatem repellendus quam, doloremque iure asperiores! Ad perspiciatis eius amet veniam reprehenderit corporis praesentium suscipit tempore maxime consequuntur!</p>
+    </section>
+  )
+}
+
+export default About
