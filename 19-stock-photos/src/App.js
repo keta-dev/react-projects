95e4be417eed5d597ff060db7b94bf632de279d7
keta-dev <89269904+keta-dev@users.noreply.github.com>
2022-12-09 06:01:11 +0100

    set up env and fetch image url

---
+++ 19-stock-photos/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './Photo'
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
+
+function App() {
+  const [loading , setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImageData = async () => {
+    let url;
+    url = `${mainUrl}${clientID}`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data);
+    } catch (err) {
+      setLoading(false)
+      console.log(err)
+    }
+  };
+
+  useEffect(() => {
+    fetchImageData();
+  }, [])
+
+  return <h2>stock photos starter</h2>
+}
+
+export default App
