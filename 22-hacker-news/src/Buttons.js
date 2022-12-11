c623e3d0965eacd6134fa78495e75fe306f79778
keta-dev <89269904+keta-dev@users.noreply.github.com>
2022-12-11 21:17:41 +0100

    hacker news

---
+++ 22-hacker-news/src/Buttons.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+const Buttons = () => {
+  const { handlePage, isLoading, page, nbPages } = useGlobalContext()
+
+  return (
+    <div className='btn-container'>
+      <button disabled={isLoading} className="btn" onClick={() => handlePage('dec')}>prev</button>
+      <p>{page + 1} of {nbPages}</p>
+      <button disabled={isLoading} className="btn" onClick={() => handlePage('inc')}>next</button>
+    </div>
+  )
+}
+
+export default Buttons
